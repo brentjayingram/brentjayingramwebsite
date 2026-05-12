c2dee02170a42d04a0ea0ddd28971d84fa71e2be
Brent Ingram <bingr001@users.noreply.github.com>
2026-05-12 11:02:36 -0400

    Comment out data_source_id output in knowledge_base.tf

--- terraform/knowledge_base.tf
+++ terraform/knowledge_base.tf
@@ -67,6 +67,6 @@ output "knowledge_base_id" {
   value = aws_bedrockagent_knowledge_base.resume_kb.id
 }
 
-output "data_source_id" {
-  value = "DATA_SOURCE_ID"
-}
+# output "data_source_id" {
+#   value = "DATA_SOURCE_ID"
+# }
